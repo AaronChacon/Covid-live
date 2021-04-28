4f9bf21c2d764a8c691fd0aaecfd74a9ea33b5c9
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-28 17:48:56 -0400

    nav styles

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { NavComponent } from './nav/nav.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,6 +14,7 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     CommonModule,
     MaterialModule,
+    RouterModule
   ],
   exports: [
     NavComponent
